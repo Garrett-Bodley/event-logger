06fd34e1af4e5715fa6c8722dc16082df01b17e6
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-02 13:19:58 -0400

    Start on #new and #create functionality.
    Created a private method to parse server log input which is used to create a new Event model instance.
    
    Also created a basic #show action, though the corresponding view still needs to be created.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,26 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    parse_log(event_params[:log_txt])
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def parse_log(text)
+    src = text.match(/\ssrc=(\S*)\s/)[1]
+    dst = text.match(/\sdst=(\S*)\s/)[1]
+    @event = Event.new(src: src, dst: dst, log_txt: text)
+    binding.pry
+  end
+
+  def event_params
+    params.require(:event).permit(:log_txt)
+  end
+
 end
