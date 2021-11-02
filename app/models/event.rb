200a4900a0aeae77a6b90d7172de9c34db8c443b
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-02 16:39:31 -0400

    Create custom error messages for attributes.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :src, presence: true
-  validates :dst, presence: true
-  validates :log_txt, presence: true, uniqueness: true
+  validates :src, presence: { message: 'Provided log must include a source IP (ex: "src=...")' }
+  validates :dst, presence: { message: 'Provided log must include a destination IP (ex: "dst=...")' }
+  validates :log_txt, presence: { message: "Form cannot be empty" }, uniqueness: { message: 'This event has already been saved in the database!' }
 end
