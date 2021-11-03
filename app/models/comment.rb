c228c509d56ac8bb0d0aa0f4a47c8840d881c7a8
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-03 17:04:56 -0400

    Enforce rubocop changes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :event
-  validates :name, presence: { message: "You must provide a name" }
-  validates :content, presence: { message: "Comment content cannot be blank" }
+  validates :name, presence: { message: 'You must provide a name' }
+  validates :content, presence: { message: 'Comment content cannot be blank' }
 end
