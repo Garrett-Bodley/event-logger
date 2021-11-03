2e9acd06f1dceba81822b7191279b4cb032f6288
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-03 13:11:24 -0400

    Fix ActiveRecord associations.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :events
+  has_many :comments
 
   validates :src, presence: { message: 'Provided log must include a source IP (ex: "src=...")' }
   validates :dst, presence: { message: 'Provided log must include a destination IP (ex: "dst=...")' }
