0c85cb6e9b103b7e92b47f8f8609be0380c24873
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-04 14:53:14 -0400

    Remove unnecessary instance variables for flash messages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   add_flash_types :updated_obj_id
-  before_action :map_flash
-
-  private
-
-  def map_flash
-    @alert = alert
-    @notice = notice
-    @updated_obj_id = updated_obj_id
-  end
 end
